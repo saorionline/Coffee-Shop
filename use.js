d61384e05966739d9858d8e5f372e8a013c77adc
Saori Isabel Tovar Niño <saorisabeltovar@gmail.com>
2023-01-04 19:09:38 -0500

    refactorizando ando

--- use.js
+++ use.js
@@ -1,28 +1,104 @@
 //Javascript
+const thirdPart = document.getElementById('logo-menu')
 const secondSection =document.getElementById('search-user-buy')
 const firstCover = document.getElementById('promo-bar');
 
+let brandIdifier
 let glassUser
 let launchNew
 
+let logoSealers = []
 let treeOfIcons = []
 let growerCards = []
+
+
+
+
+//menu of options
+class logoSealer {
+    constructor(name) {
+        this.name = name
+    }
+}
 //interactivity
 class treeOfIcon {
     constructor(name){
         this.name = name
     }
 }
-let firstDesign = new treeOfIcon('Glam Beige')
-treeOfIcons.push(firstDesign)
+
+
 // discounts
 class growerCard {
     constructor(name) {
         this.name = name
     }
 }
+
+let firstLogoPart = new logoSealer('Centered')
+let firstDesign = new treeOfIcon('Glam Beige')
 let firstCard = new growerCard('January')
+
+logoSealers.push(firstLogoPart)
+treeOfIcons.push(firstDesign)
 growerCards.push(firstCard)
+
+function pasteLogo() {
+    logoSealers.forEach((logoSealer) => {
+        
+    brandIdifier =`
+            <ul class="nav-long-section">
+                <li id="highInt">Tienda
+                    <!--  <ul class="inner-menu">
+                            <li id="me">Café Especial
+                                <ul class="inner-menu">
+                                    <li id="me">Blends</li>
+                                    <li id="me">Single Origin</li>
+                                    <li id="me">Cold Bred</li>
+                                    <li id="me">Kits</li>
+                                    <li id="me">Métodos</li>
+                                </ul>
+                            </li>
+                            <li>Merch</li>
+                        </ul> -->
+                </li>      
+                    <li> <canvas class="cube"></canvas></li>                    
+                    <li class="logo-tibetano">
+                        <img src="./assets/tibetano/main-logo.jpg" alt="this is just to learn">
+                    </li>
+                    <li>
+                        <a>Nosotros</a>
+                    </li>
+                    <li>
+                        <a >Reaprender</a>
+                    </li> 
+                    <li>
+                        <a >Visítanos</a>
+                    </li>       
+                    <li> <canvas class="cube"></canvas></li>  
+                    </ul>`
+                
+    thirdPart.innerHTML += brandIdifier
+    })
+}
+
+function orderBy() {
+    //Search, user and buy bag
+    treeOfIcons.forEach((treeOfIcon) => {
+        // order one
+        glassUser = `
+        <ul class="nav-right-section">
+            <li class="main-input-box">    
+                <span class="s-icon" ></span>
+                <input type="text" placeholder="Buscar" >
+            </li>  
+            <li class="u-icon"></li>
+            <li class="b-icon"></li> 
+        </ul>`
+    secondSection.innerHTML += glassUser
+    })
+    pasteLogo()
+}
 function promoBar() {
 
     //Promotions and interactive activities bar
@@ -40,26 +116,6 @@ function promoBar() {
     orderBy()
 }
 
-function orderBy() {
-    //Search, user and buy bag
-    treeOfIcons.forEach((treeOfIcon) => {
-        // order one
-        glassUser = `
-        <ul class="nav-right-section">
-            <li class="main-input-box">    
-                <span class="s-icon" ></span>
-                <input type="text" placeholder="Buscar" >
-            </li>
-            
-            <li>
-                <i class="u-icon" ></i>
-            </li>
-            <li>
-                <i class="b-icon" ></i>
-            </li> 
-        </ul>`
-    secondSection.innerHTML += glassUser
-    })
-}
+
 
 window.addEventListener('load', promoBar)
